bbb68d029d1f3af7756899313b18f6a811ea5f0d
Season <277594531@qq.com>
2016-07-09 18:08:43 +0800

    test

--- src/com/test/demo/Hello.java
+++ src/com/test/demo/Hello.java
@@ -2,9 +2,11 @@ package com.test.demo;
 
 /**
  * Created by yexianshan on 2016/7/9.
+ * so it's good
  */
 public class Hello {
     public static void main(String[] args){
-        System.out.print("hello");
+        System.out.println("hello");
+        System.out.println("Tom, what are you doing?");
     }
 }
