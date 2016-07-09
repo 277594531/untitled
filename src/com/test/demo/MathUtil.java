b6e6b24675d8383fb58750c85b2edd21e2652e5d
Season <277594531@qq.com>
2016-07-09 18:28:05 +0800

    test

--- src/com/test/demo/MathUtil.java
+++ src/com/test/demo/MathUtil.java
@@ -7,4 +7,8 @@ public class MathUtil {
     public static double Add(double v1, double v2){
         return v1 + v2;
     }
+
+    public static void main(String[] args){
+        System.out.println("come on baby.");
+    }
 }
