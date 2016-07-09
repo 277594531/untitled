672b617bfbfb327431a81a1d97725f4bb2bf0d19
Season <277594531@qq.com>
2016-07-09 18:20:50 +0800

    test

--- src/com/test/demo/Hello.java
+++ src/com/test/demo/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("Tom, what are you doing?");
+        System.out.println(MathUtil.Add(1.1, 2.2));
     }
 }
---
+++ src/com/test/demo/MathUtil.java
@@ -0,0 +1,10 @@
+package com.test.demo;
+
+/**
+ * Created by yexianshan on 2016/7/9.
+ */
+public class MathUtil {
+    public static double Add(double v1, double v2){
+        return v1 + v2;
+    }
+}
